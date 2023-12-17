a31f7828aaf51a6a35495556fe6f4cfd48cd88b9
putrija <putrijamalau23@gmail.com>
2023-12-17 12:09:06 +0700

    mengubah layout lagi untuk detail tugas

---
+++ app/src/main/java/com/example/tugasbesarpemrogramanmobile/DetailTugasActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tugasbesarpemrogramanmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailTugasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.detail_tugas)
+    }
+}
