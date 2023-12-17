727ea68084c04d4b7c6a4bc5b808ddef12a4b842
AgnesTS <agnestryanisim@gmail.com>
2023-12-18 00:54:43 +0700

    back

--- app/src/main/java/com/example/tugasbesarpemrogramanmobile/AddtugasActivity.kt
+++ app/src/main/java/com/example/tugasbesarpemrogramanmobile/AddtugasActivity.kt
@@ -9,8 +9,6 @@ class AddtugasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_tugas)
-
-        Button
     }
 
 }
